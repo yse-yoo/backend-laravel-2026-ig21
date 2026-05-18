bcae7fb1de86a0fefbf84ec7546a39a9671e4244
yse-yoo <youhei_yoshikawa@yse-c.net>
2026-05-18 15:08:20 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/about', function () {
     return view('about');
