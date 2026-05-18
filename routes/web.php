324eb24b40f24fb9aa47beb943c52eb9c1398089
yse-yoo <youhei_yoshikawa@yse-c.net>
2026-05-18 15:12:15 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [HomeController::class, 'about']);
 
 Route::get('/product/{id}', function ($id) {
     // TODO: データベースで検索
